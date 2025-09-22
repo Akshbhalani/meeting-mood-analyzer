36f7e8cedf831822f5c830591e1512fa10927a11
akshbhalani <akshbhalani119@gmail.com>
2025-09-22 13:03:30 +0200

    Clean version: add source files only

--- test_env.py
+++ test_env.py
@@ -1,18 +1,3 @@
-# import torch
-# import transformers
-# import nltk
-# import pandas as pd
-# import matplotlib.pyplot as plt
-
-# print("Torch:", torch.__version__)
-# print("Transformers:", transformers.__version__)
-# print("NLTK:", nltk.__version__)
-# print("Pandas:", pd.__version__)
-
-
-# Phase 3
-
-
 import nltk
 import spacy
 import textblob
